86ccc43917676016838c3af67fef2dc1b2275b34
DeniZayn <0990759@gmail.com>
2022-01-29 22:01:02 +0300

    HW Stopwatch

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stopwatch.data.TimestampProviderImpl
+import com.example.stopwatch.data.calculates.ElapsedTimeCalculator
+import com.example.stopwatch.data.calculates.StopwatchStateCalculator
+import com.example.stopwatch.databinding.ActivityMainBinding
+import com.example.stopwatch.viewmodel.StopwatchStateHolder
+import com.example.stopwatch.viewmodel.StopwatchViewModel
+import com.example.stopwatch.viewmodel.StopwatchViewModelFactory
+import kotlinx.coroutines.flow.collect
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val timestampProvider = TimestampProviderImpl()
+    private lateinit var stopwatchModel: StopwatchViewModel
+
+    companion object {
+        const val TIMER_1 = 1
+        const val TIMER_2 = 2
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        stopwatchModel = StopwatchViewModelFactory(
+            stopwatchStateHolder = StopwatchStateHolder(
+                StopwatchStateCalculator(
+                    timestampProvider,
+                    ElapsedTimeCalculator(timestampProvider)
+                ),
+                ElapsedTimeCalculator(timestampProvider)
+            )
+        ).create(StopwatchViewModel::class.java)
+
+        stopwatchModel.getLiveData().observe(this) {
+            when (it.first) {
+                TIMER_1 -> binding.textTime1.text = it.second
+                TIMER_2 -> binding.textTime2.text = it.second
+            }
+        }
+
+        binding.buttonStart1.setOnClickListener {
+            stopwatchModel.start(TIMER_1)
+        }
+        binding.buttonPause1.setOnClickListener {
+            stopwatchModel.pause(TIMER_1)
+        }
+        binding.buttonStop1.setOnClickListener {
+            stopwatchModel.stop(TIMER_1)
+        }
+
+        binding.buttonStart2.setOnClickListener {
+            stopwatchModel.start(TIMER_2)
+        }
+        binding.buttonPause2.setOnClickListener {
+            stopwatchModel.pause(TIMER_2)
+        }
+        binding.buttonStop2.setOnClickListener {
+            stopwatchModel.stop(TIMER_2)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/stopwatch/data/TimestampProviderImpl.kt
@@ -0,0 +1,6 @@
+package com.example.stopwatch.data
+
+class TimestampProviderImpl: TimestampProvider {
+
+    override fun getMilliseconds(): Long = System.currentTimeMillis()
+}
